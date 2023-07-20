9371271295bd419e7672322342d928c21aafc28d
Steven <fanaihua@unicon-sky.com.cn>
2023-07-20 10:19:02 +0800

    练习：如何给一个应用中的函数添加授权

--- home/views.py
+++ home/views.py
@@ -4,3 +4,11 @@ from datetime import datetime
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/admin')
+def new_function(request):
+    return render(request, 'home/test.html')
